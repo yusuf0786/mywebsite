66473496305ee3d587ee41aaf1177ce68db57daa
yusuf <74721122+yusuf0786@users.noreply.github.com>
2023-08-08 00:00:39 +0530

    home page changes & portfolio page deleted & work page added

--- work/js/script.js
+++ work/js/script.js
@@ -1,7 +1,33 @@
 // targetting the elements
+let click_to_main = document.querySelector("#click_to_main")
+let sec_main = document.querySelector("#sec_main")
+let layout_anchors = document.querySelectorAll("#sec_main a")
+let layout_imgs = document.querySelectorAll("#sec_main a img")
+let back_btn = document.querySelector(".back_btn")
 let mynav = document.querySelector("#mynav ul")
 let nav_btn = document.querySelector(".btn_toggle")
 let nav_toggled = false
+
+// click function
+click_to_main.addEventListener('click', function(){
+    sec_main.classList.remove("main_sec_anim2")
+    sec_main.style.display = "block"
+    sec_main.classList.add("main_sec_anim")
+
+    modalOpenNecessoryEffect()
+
+    mainsection_mause(layout_anchors, layout_imgs)
+})
+
+back_btn.addEventListener('click', function(){
+    sec_main.classList.remove("main_sec_anim")
+    sec_main.classList.add("main_sec_anim2")
+    setTimeout(function(){
+        sec_main.style.display = "none"
+
+        modalCloseNecessoryEffect()
+    }, 500)
+})
 
 nav_btn.addEventListener('click', function(){
     !nav_toggled ? nav_open() : nav_close()
@@ -62,3 +88,30 @@ function modalCloseNecessoryEffect() {
     document.body.style.paddingRight = ""
     document.querySelector("header").style.paddingRight = ""
 }
+
+
+// GSAP
+let sliderImgImages = document.querySelectorAll(".slider-img a")
+
+let lt = gsap.timeline({
+  defaults:{duration:1},
+  scrollTrigger: {
+    trigger: "body",
+    toggleActions: "restart pause reverse pause",
+    start: "1% top",
+    end: "+=5000",
+    scrub: 1,
+    pin: ".slider-img",
+    onEnter: () => {
+        gsap.to(".slider-img-container", {opacity:1})
+        gsap.to(".sec1_inside", {opacity:0})
+        gsap.to(".sec1 > iframe", {opacity:0})
+    },
+    onLeaveBack: () => {
+        gsap.to(".slider-img-container", {opacity:0})
+        gsap.to(".sec1_inside", {opacity:1})
+        gsap.to(".sec1 > iframe", {opacity:1})
+    },
+  }
+})
+lt.fromTo(".slider-img", {rotateY: "0deg"}, {rotateY: "360deg"})
